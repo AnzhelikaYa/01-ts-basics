74b68ad7280e247310d595ab0bf0ce0645476f66
Anzhelika Ya <didealika@gmail.com>
2025-11-23 02:46:57 +0200

    task-6

--- src/task-6.ts
+++ src/task-6.ts
@@ -3,6 +3,6 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-getFirstElement([1, 2, 3]);           // 1
-getFirstElement(["a", "b", "c"]);     // "a"
-getFirstElement([true, false, true]); // true
+const num = getFirstElement<number>([1, 2, 3]);              // number
+const str = getFirstElement<string>(["a", "b", "c"]);        // string
+const bool = getFirstElement<boolean>([true, false, true]);  // boolean
